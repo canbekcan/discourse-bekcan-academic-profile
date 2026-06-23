52b12508c5137a0ceeb4b588400e88e9a1716f4d
canbekcan <can@canbekcan.com>
2026-06-23 07:55:49 +0300

    v0.0.5

--- plugin.rb
+++ plugin.rb
@@ -5,11 +5,22 @@
 # url: https://github.com/canbekcan/discourse-bekcan-academic-profile
 
 after_initialize do
-  [:academic_title, :main_field, :orcid_id].each { |p| DiscoursePluginRegistry.permitted_user_update_params << p }
+  # Ensure custom user field definitions exist (Admin UI integration)
+  [:academic_title, :main_field, :orcid_id].each do |field_name|
+    UserCustomFieldDefinition.find_or_create_by(name: field_name.to_s) do |f|
+      f.field_type = 'text'
+    end
+  end
+
+  # Register fields for user updates
+  [:academic_title, :main_field, :orcid_id].each do |p|
+    DiscoursePluginRegistry.permitted_user_update_params << p
+  end
   DiscoursePluginRegistry.permitted_user_update_params << { sub_fields: [] }
 
-  add_to_serializer(:user, :academic_title) { object.academic_profile_field&.academic_title }
-  add_to_serializer(:user, :main_field) { object.academic_profile_field&.main_field }
-  add_to_serializer(:user, :sub_fields) { object.academic_profile_field&.sub_fields }
-  add_to_serializer(:current_user, :orcid_id) { object.academic_profile_field&.orcid_id }
+  # Serializers now map from the custom_fields hash provided by the User model
+  add_to_serializer(:user, :academic_title) { object.custom_fields['academic_title'] }
+  add_to_serializer(:user, :main_field) { object.custom_fields['main_field'] }
+  add_to_serializer(:user, :sub_fields) { object.custom_fields['sub_fields'] }
+  add_to_serializer(:current_user, :orcid_id) { object.custom_fields['orcid_id'] }
 end
