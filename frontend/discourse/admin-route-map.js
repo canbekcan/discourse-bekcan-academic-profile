b1c49c17f0fad18b544df59b1a1bc7287746ff21
canbekcan <can@canbekcan.com>
2026-06-24 07:27:39 +0300

    v0.1.5.2

--- frontend/discourse/admin-route-map.js
+++ frontend/discourse/admin-route-map.js
@@ -1,4 +1,4 @@
 /** @ts-check */
 export default function () {
-  this.route("academic-profile"); // Nested olmamalı!
+  this.route("adminPlugins", { path: "/admin/plugins" }, function() { this.route("academic-profile"); });
 }
